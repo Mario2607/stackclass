bf97fe483497b83db94a40f5f4802ce60a50a6ec
mmilkov <mario.milkov26@gmail.com>
2025-01-22 13:58:24 +0100

    Exceptions done with comments

--- StackEmptyException.java
+++ StackEmptyException.java
@@ -1,4 +1,11 @@
-public class StackEmptyException extends Exception{
+/**
+ * Exception thrown when attempting to pop or peek an element from an empty stack.
+ */
+public class StackEmptyException extends Exception {
+    /**
+     * Constructor for StackEmptyException.
+     * @param message The exception message indicating the stack is empty.
+     */
     public StackEmptyException(String message) {
         super(message);
     }
--- StackFullException.java
+++ StackFullException.java
@@ -1,4 +1,11 @@
-public class StackFullException extends Exception{
+/**
+ * Exception thrown when attempting to push an element into a full stack.
+ */
+public class StackFullException extends Exception {
+    /**
+     * Constructor for StackFullException.
+     * @param message The exception message indicating the stack is full.
+     */
     public StackFullException(String message) {
         super(message);
     }
